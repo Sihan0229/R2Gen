77e05fa9da7748a8c75976e86217bbf0248d2183
sihan0229 <1109458800@qq.com>
2024-04-25 21:21:18 +0800

    class notes

--- models/r2gen.py
+++ models/r2gen.py
@@ -19,13 +19,19 @@ class R2GenModel(nn.Module):
             self.forward = self.forward_mimic_cxr
 
     def __str__(self):
+        # filter()：这是 Python 内置的函数，用于过滤序列中的元素，返回一个迭代器，其中包含使得过滤函数返回 True 的元素。
         model_parameters = filter(lambda p: p.requires_grad, self.parameters())
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
 
     def forward_iu_xray(self, images, targets=None, mode='train'):
+        # 这行代码调用visual_extractor来提取图像的特征。
+        # images[:, 0]选择了输入图像中的第一个图像，然后将其传递给特征提取器。
         att_feats_0, fc_feats_0 = self.visual_extractor(images[:, 0])
+        # att_feats_0和fc_feats_0分别是提取出的注意力特征和全连接特征。
+        # 第二个图像
         att_feats_1, fc_feats_1 = self.visual_extractor(images[:, 1])
+        # 全连接特征拼接（dim=1）和注意力特征拼接
         fc_feats = torch.cat((fc_feats_0, fc_feats_1), dim=1)
         att_feats = torch.cat((att_feats_0, att_feats_1), dim=1)
         if mode == 'train':
